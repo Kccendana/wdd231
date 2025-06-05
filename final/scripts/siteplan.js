134898ebd2fc2a503e9192692684bf816cc04bd6
kcccendana <kc_telyn@yahoo.com>
2025-06-06 06:51:52 +0900

    siteplan

---
+++ final/scripts/siteplan.js
@@ -0,0 +1,5 @@
+const year = document.querySelector("#year");
+year.textContent = new Date().getFullYear();
+
+const lastModified = document.getElementById("lastModif")
+lastModified.textContent = document.lastModified;
